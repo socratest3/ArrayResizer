860c5d332ac029f419b1cd44cde3521c2f0cc4da
socratest3 <socratest3@nycstudents.net>
2025-05-04 00:54:33 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
+public class Main
+{
+   public static void main(String[]args)
+   {
+       int[][] arr =
+       {
+         {2,1,0},
+         {1,3,2},
+         {0,0,0},
+         {4,5,6};
+       };
+       System.out.println(ArrayResizer.isNonZeroRow(arr,0));
+       System.out.println(ArrayResizer.isNonZeroRow(arr,1));
+       System.out.println(ArrayResizer.isNonZeroRow(arr,2));
+       System.out.println(ArrayResizer.isNonZeroRow(arr,3));
 
+int [][] result = ArrayResizer.resize(arr);
+for(int[] row : result)
+{
+   for(int col : row) System.out.print(col + " ");
+   System.out.println();
+}
+   }
+}
